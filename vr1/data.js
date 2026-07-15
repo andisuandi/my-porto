92b638d3f16499481daa17b8e42fe4add02ae6ca
andisuandi <73454524+andisuandi@users.noreply.github.com>
2026-07-15 11:48:24 +0700

    Add VR by marzipano

---
+++ vr1/data.js
@@ -0,0 +1,86 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-timothy-unsplash",
+      "name": "timothy-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5872316345895747,
+          "pitch": -0.10281362447873477,
+          "rotation": 0,
+          "target": "1-blabla5-winter-2383930_1920"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-blabla5-winter-2383930_1920",
+      "name": "blabla5-winter-2383930_1920",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "yaw": -2.7329822714757217,
+        "pitch": -0.22061060679150835,
+        "fov": 1.415963136095193
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.823964249828581,
+          "pitch": -0.4217632345016149,
+          "rotation": 1.5707963267948966,
+          "target": "0-timothy-unsplash"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.620187961136784,
+          "pitch": -0.4206964169443772,
+          "title": "INFO",
+          "text": "Sekilas Info tempatkan disini"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
